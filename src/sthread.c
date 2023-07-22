f0f67f53b4c736e8ccd29897ecaafa75bc784e35
hsiam261 <hsiam261@gmail.com>
2023-07-23 01:16:28 +0600

    add sthread_join

--- src/sthread.c
+++ src/sthread.c
@@ -18,6 +18,7 @@ typedef struct sthread_attr {
 
 typedef struct sthread {
 	int return_value;
+	void* result;
 	uint32_t lock;
 	uint64_t tid;
 	int pidfd;
@@ -54,6 +55,12 @@ void thread_done(sthread_t tcb, int ret_val) {
 	tcb->lock = 1;
 	tcb->return_value = ret_val;
 	syscall(SYS_futex, &tcb->lock, FUTEX_WAKE, INT_MAX, NULL, NULL, 0);
+	syscall(SYS_exit, 0);
+}
+
+void thread_func_wrapper(void *(*func)(void *), void * args, sthread_t tcb) {
+	func(args);
+	thread_done(tcb, 0);
 }
 
 void sthread_create(sthread_t* thread, sthread_attr* thread_attr, void * (*func)(void *), void* args) {
@@ -64,6 +71,7 @@ void sthread_create(sthread_t* thread, sthread_attr* thread_attr, void * (*func)
 	void* stack_ptr = allocate_stack(thread_attr);
 	*thread = stack_ptr;
 	sthread_t tcb = stack_ptr;
+	tcb->lock = 0;
 
 	int clone_flags = (CLONE_VM | CLONE_THREAD | CLONE_SIGHAND | 
 			CLONE_FILES | CLONE_FS | CLONE_PARENT_SETTID | 
@@ -83,24 +91,31 @@ void sthread_create(sthread_t* thread, sthread_attr* thread_attr, void * (*func)
 	pid_t pid;
 	
 	__asm__ volatile(
-		"movq %3, %%rbx\n\t"
+		"movq %6, %%rbx\n\t"    // start thread func (wrapper over func)
+		"movq %3, %%r12\n\t"    // start thread func ARGUMENT 0: func
+		"movq %7, %%r13\n\t"    // start thread func ARGUMENT 1: args
+		"movq %8, %%r14\n\t"	// start thread func ARGUMENT 2: tcb 
 		"movq $0, %%rax\n\t"
-		"mov %1, %%eax\n\t"
-		"movq %4, %%rdi\n\t"
-		"movq %5, %%rsi\n\t"
-		"syscall\n\t"		/* Linux/i386 system call */
-		"testq %%rax,%%rax\n\t"	/* check return value */
-		"jne 1f\n\t"		/* jump if parent */
-		"callq *%%rbx\n\t"		/* start subthread function */
-		"movl %2,%0\n\t"
-		"syscall\n"		/* exit system call: exit subthread */
+		"mov %1, %%eax\n\t"     // SYSTEM CALL NUMBER : SYS_clone3
+		"movq %4, %%rdi\n\t"    // zeroth argument to clone3: &cl_args
+		"movq %5, %%rsi\n\t"    // first argument to clone3: sizeof(cl_args) 
+		"syscall\n\t"		// Linux/amd64 system call */
+		"testq %%rax,%%rax\n\t"	// check return value */
+		"jne 1f\n\t"		// jump if parent */
+		"movq %%r12, %%rdi\n\t"    // FUNCTION ARGUMENT 0: func
+		"movq %%r13, %%rsi\n\t"    // FUNCTION ARGUMENT 1: args
+		"movq %%r14, %%rdx\n\t"	// FUNCTION ARGUMENT 2: tcb 
+		"callq *%%rbx\n\t"	// start subthread function */
 		"1:\t"
 		:"=a" (pid)
 		:"i" (SYS_clone3),"i" (__NR_exit),
 		"r" (func),
 		"r" (&cl_args),
-		"r" (sizeof(cl_args))
-		:"rdi", "rsi"
+		"r" (sizeof(cl_args)),
+		"r" (&thread_func_wrapper),
+		"r" (args),
+		"r" (tcb)
+		:"rdi", "rsi", "r12", "r13", "r14"
 	);
 
 	if(pid < 0) {
@@ -108,6 +123,12 @@ void sthread_create(sthread_t* thread, sthread_attr* thread_attr, void * (*func)
 	}
 
 	printf("Here2\n");
+}
+
+int sthread_join(sthread_t thread, void** ret_val) {
+	syscall(SYS_futex, &thread->lock, FUTEX_WAIT, 0, NULL, NULL, 0);
+	if(ret_val) *ret_val = thread->result;
+	return thread->return_value;
 }
 
 void* child_proc(void * args) {
@@ -121,8 +142,8 @@ int main() {
 
 	sthread_t thread1, thread2;
 	sthread_create(&thread1, NULL, child_proc, NULL);
-	sleep(5);
+	sthread_join(thread1, NULL);
 	sthread_create(&thread2, NULL, child_proc, NULL);
-	sleep(5);
+	sthread_join(thread2, NULL);
 	return 0;
 }
