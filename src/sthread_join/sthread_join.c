6d12fe59a663395ce5f4f6c5ff6b1ca0fcdf4590
hsiam261 <hsiam261@gmail.com>
2024-12-28 03:05:51 +0600

    add commment about multiple joins

--- src/sthread_join/sthread_join.c
+++ src/sthread_join/sthread_join.c
@@ -9,6 +9,6 @@
 int sthread_join(sthread_t thread, void** ret_val) {
 	syscall(SYS_futex, &thread->lock, FUTEX_WAIT, 0, NULL, NULL, 0);
 	if(ret_val) *ret_val = thread->result;
-	deallocate_stack(&thread->thread_attributes);
+	deallocate_stack(&thread->thread_attributes); //multiple joins on same thread will break things
 	return 0;
 }
