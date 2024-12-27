29eba06c8b8c4aa60044927d15ed807eda7ba5ad
hsiam261 <hsiam261@gmail.com>
2024-12-28 02:47:03 +0600

    store sthread_attr in sthread_t object
    Also stop writing to sthread_attr in allocate stack.
    It might cause issue when writing to default_sthread_attr

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -8,15 +8,10 @@ void* allocate_stack(sthread_attr* thread_attr) {
 		thread_attr = &default_sthread_attr;
 	}
 
-	if(thread_attr->stack_ptr) {
-		return thread_attr->stack_ptr;
-	}
-
 	void* stack_ptr = mmap(NULL, thread_attr->stack_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE| MAP_STACK, 0, 0);
 	if(stack_ptr == MAP_FAILED) {
 		return NULL;
 	}
 
-	thread_attr->stack_ptr = stack_ptr;
 	return stack_ptr;
 }
