077ed3742eddb090a0fde84221827ca475c1fe85
hsiam261 <hsiam261@gmail.com>
2024-12-28 03:05:13 +0600

    deallocate stack when joinning

--- src/sthread_join/sthread_join.c
+++ src/sthread_join/sthread_join.c
@@ -4,8 +4,11 @@
 #include <linux/futex.h>
 #include <linux/sched.h>
 
+#include "../stack/stack.h"
+
 int sthread_join(sthread_t thread, void** ret_val) {
 	syscall(SYS_futex, &thread->lock, FUTEX_WAIT, 0, NULL, NULL, 0);
 	if(ret_val) *ret_val = thread->result;
-	return thread->return_value;
+	deallocate_stack(&thread->thread_attributes);
+	return 0;
 }
